91503fbfd5f6ab1e74eec3a5c791d4478d2c8664
Jason Belgard <belgard.jason@gmail.com>
2019-06-17 19:12:05 -0500

    final fix

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -13,11 +13,13 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
       return {
-        ...state, count: initialState.count++
+        ...state, 
+        count: initialState.count++
       }
     case DECREMENT:
       return {
-        ...state, count: initialState.count--
+        ...state, 
+        count: initialState.count--
       }
     default:
       return state;
